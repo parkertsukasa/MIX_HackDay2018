276456a4d5934416d51786ea1baf56ec01b180cf
TsukasaSumizono <freddiesumiz.3@gmail.com>
2018-12-16 02:36:51 +0900

    UI完成

--- Assets/Parker/Scripts/EffectMenuManager.cs
+++ Assets/Parker/Scripts/EffectMenuManager.cs
@@ -12,12 +12,25 @@ public class EffectMenuManager : MonoBehaviour
 	[SerializeField]
 	private Canvas soundCanvas;
 
+	[SerializeField]
+	private Image menuButton;
+
+	[SerializeField]
+	private Sprite cross;
+
+	[SerializeField]
+	private Sprite gear;
+
+	[SerializeField]
+	private GameObject uiAll;
+
 	// Use this for initialization
 	void Start () 
 	{
 		effectCanvas.enabled = false;
 		soundCanvas.enabled = true;
 		menuBackAnimator.SetBool("MenuEnabled", false);
+		uiAll.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -31,18 +44,32 @@ public class EffectMenuManager : MonoBehaviour
 		if (effectCanvas.enabled == true) // すでにMenuが出ている時
 		{
 			menuBackAnimator.SetBool("MenuEnabled", false);
-			Invoke("EffectMenuOut", 1.0f);
+			UIOut();
+			Invoke("EffectMenuOut", 0.35f);
 		}
 		else // Menuが出ていない時
 		{
 			effectCanvas.enabled = true;
 			menuBackAnimator.SetBool("MenuEnabled", true);
+			menuButton.sprite = cross;
+			Invoke("UIIn", 0.35f);
 		}
+	}
+
+	void UIIn()
+	{
+		uiAll.SetActive(true);
+	}
+
+	void UIOut()
+	{
+		uiAll.SetActive(false);
 	}
 
 	void EffectMenuOut()
 	{
 		effectCanvas.enabled = false;// !effectCanvas.enabled;
+		menuButton.sprite = gear;
 	}
 
 }
