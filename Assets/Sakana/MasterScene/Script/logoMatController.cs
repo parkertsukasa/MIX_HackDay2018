172be1c4e3d9d83a3f93ee8b1f6161fe91db6577
KeitoTakaishi <keitoncis@icloud.com>
2018-12-16 06:11:46 +0900

    seconds

---
+++ Assets/Sakana/MasterScene/Script/logoMatController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class logoMatController : MonoBehaviour {
+
+    //public Material material;
+    public GameObject audio;
+    AudioSpectrum _as;
+    void Start () {
+        _as = audio.GetComponent<AudioSpectrum>();
+		
+	}
+	
+	void Update () {
+        this.GetComponent<MeshRenderer>().material.color = new Color(_as.MeanLevels[0] * 3.0f, 1.0f, 1.0f);
+        
+	}
+}
---
+++ Assets/Sakana/unity-audio-spectrum/AudioVisualizeTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisualizeTest : MonoBehaviour {
+
+    public GameObject Audioplayer;
+    public GameObject obj;
+    public Material mat;
+
+    private AudioSpectrum _as;
+    private List<GameObject> _objs = new List<GameObject>();
+
+
+    private int _num;
+    float _width = 15.0f;
+    float _start;
+    float _step;
+
+
+    void Start () {
+        _as = Audioplayer.GetComponent<AudioSpectrum>();
+        _num = _as.MeanLevels.Length;
+
+        _start = -1.0f * _width / 2.0f;
+        _step = _width / _num;
+
+
+
+
+        //for (int i = 0; i < _num; i++) {
+        //    _objs.Add(Instantiate(obj));
+        //    _objs[i].transform.position = new Vector3(_start + _step*i, 0.0f, 0.0f);
+        //    _objs[i].transform.rotation = Quaternion.identity;
+        //}
+	}
+	
+	void Update () {
+        for (int i = 0; i < _num; i++) {
+            mat.SetFloatArray("_vol", _as.MeanLevels);
+            //_objs.Add(Instantiate(obj));
+            //_objs[i].transform.localScale = new Vector3(_start + _step * i, _as.MeanLevels[i] * 10.0f, 0.0f);
+            //_objs[i].transform.position = new Vector3(_start + _step * i, 0.0f, 0.0f);
+            //_objs[i].transform.rotation = Quaternion.identity;
+        }
+        Debug.Log(_as.MeanLevels[0]);
+    }
+}
